7181a61ec98e007f7c66e7648b13505d2f9459f8
chi <chiguanglong0@gmail.com>
2019-11-12 00:19:43 +0900

    test circleci

--- application/myblog/settings/ci.py
+++ application/myblog/settings/ci.py
@@ -3,8 +3,8 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myblog',
-        'USER': 'root',
+        'NAME': 'circle_test',
+        'USER': 'docker',
         'PASSWORD': 'docker',
         'HOST': '127.0.0.1',
         'PORT': '3306',
