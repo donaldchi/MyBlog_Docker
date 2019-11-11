be08309e126545aee685500ad90a1753009537f3
chi <chiguanglong0@gmail.com>
2019-11-12 00:31:33 +0900

    test circleci

--- application/myblog/settings/ci.py
+++ application/myblog/settings/ci.py
@@ -6,7 +6,7 @@ DATABASES = {
         'NAME': 'circle_test',
         'USER': 'docker',
         'PASSWORD': 'docker',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
