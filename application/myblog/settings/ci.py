921e51d0a4903d67e572d51bfa9493d11c46c388
chi <chiguanglong0@gmail.com>
2019-11-12 22:20:44 +0900

    test circleci

--- application/myblog/settings/ci.py
+++ application/myblog/settings/ci.py
@@ -1,12 +1,18 @@
 from .base import *
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myblog',
         'USER': 'root',
         'PASSWORD': 'docker',
-        'HOST': 'db',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
